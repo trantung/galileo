72c35962ba2df6aaa16e211c8cea476b0d80d2d8
dxnm <2711sq@gmail.com>
2016-01-22 15:54:15 +0700

    fix menu

--- app/views/site/common/top.blade.php
+++ app/views/site/common/top.blade.php
@@ -9,14 +9,14 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="logo" href="{{ url('/') }}">
+				<a class="logo" href="{{ action('SiteIndexController@index') }}">
 					<img src="{{ url('/assets/images/logo.png') }}" />
 				</a>
 			</div>
 			<div class="col-sm-9">
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
-						<li {{ checkActive() }}><a href="{{ url('/') }}">{{ trans('captions.home') }}</a></li>
+						<li {{ checkActive() }}><a href="{{ action('SiteIndexController@index') }}">{{ trans('captions.home') }}</a></li>
 						<li {{ checkActive(LaravelLocalization::transRoute('routes.about')) }}><a href="{{ action('AboutController@index') }}">{{ trans('captions.aboutus') }}</a></li>
 						<li {{ checkActive('tin-tuc') }}><a href="./">Tin tức</a></li>
 						<li {{ checkActive('tuyen-dung') }}><a href="./">Tuyển dụng</a></li>
