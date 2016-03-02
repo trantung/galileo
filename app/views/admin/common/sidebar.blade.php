696edbc16918d57d95c84df88b0d9d4eab321b79
trantung <trantunghn196@gmail.com>
2016-03-02 12:00:38 +0700

    fix

--- app/views/admin/common/sidebar.blade.php
+++ app/views/admin/common/sidebar.blade.php
@@ -4,12 +4,12 @@
 		<!-- sidebar menu: : style can be found in sidebar.less -->
 		<ul class="sidebar-menu">
 			<li class="header">Menu</li>
-			<li><a href="{{ action('NewsController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Quản lý tin</span></a></li>
-			<li><a href="{{ action('NewsTypeController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Quản thể loại tin</span></a></li>
-			<li><a href="{{ action('AdminTypeAboutController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Loại giới thiệu</span></a></li>
-			<li><a href="{{ action('AdminAboutUsController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Giới thiệu</span></a></li>
-			<li><a href="{{ action('DesContentController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Giới thiệu trang chủ</span></a></li>
-			<li><a href="{{ action('AdminIntroduceController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Introduce</span></a></li>
+			<li><a href="{{ action('NewsController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Tin chi tiết</span></a></li>
+			<li><a href="{{ action('NewsTypeController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Danh mục Tin</span></a></li>
+			<li><a href="{{ action('AdminTypeAboutController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Trang giới thiệu</span></a></li>
+			<li><a href="{{ action('AdminAboutUsController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Chi tiết trang giới thiệu</span></a></li>
+			<li><a href="{{ action('DesContentController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Giới thiệu chung trang chủ</span></a></li>
+			<li><a href="{{ action('AdminIntroduceController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Giới thiệu chi tiết trang chủ</span></a></li>
 			<li><a href="{{ action('AdminContactController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Liên hệ</span></a></li>
 			<li><a href="{{ action('AdminContactController@feedback') }}"><i class="fa fa-newspaper-o"></i> <span>Feedback</span></a></li>
 			<li><a href="{{ action('AdminSlideController@index') }}"><i class="fa fa-newspaper-o"></i> <span>Slide</span></a></li>
