2e8c073da3f86a6d1ae83375d2eb1b35c44cdb83
Tan Nguyen <tantanb2@gmail.com>
2018-01-18 15:04:00 +0700

    fix

---
+++ app/views/admin/common/sidebar.blade.php
@@ -0,0 +1,64 @@
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+            <li class="header">Menu</li>
+            <li>
+                <a href="{{ action('ManagerPartnerController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý đối tác</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('ManagerCenterController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý các trung tâm</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('ManagerUserController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý nhân viên trung tâm</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('SubjectController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý các môn học</span>p
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('ClassController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý các lớp học</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('LevelController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý các trình độ</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ action('DocumentController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý các học liệu</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="{{ action('AdminController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý admin</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="{{ action('StudentController@index') }}">
+                    <i class="fa fa-newspaper-o"></i> 
+                    <span>Quản lý học sinh</span>
+                </a>
+            </li>
+
+        </ul>
+    </section>
+</aside>
