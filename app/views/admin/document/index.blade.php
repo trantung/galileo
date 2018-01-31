1e18d6f7daa1c513548cafeb98395850d8bb457e
tungtt2 <trantunghn196@gmail.com>
2018-01-31 14:32:00 +0700

    fix

--- app/views/admin/document/index.blade.php
+++ app/views/admin/document/index.blade.php
@@ -88,7 +88,7 @@ Danh sách học liệu
                             {{ Common::getObject($documentD, 'code') }}
                             @if(renderUrlByPermission('DocumentController@getPrint', 'print', ''))
                                 @if( Common::getObject($documentP, 'file_url') )
-                                    <a type="button" class="btn" onclick="printJS({printable: '{{ Common::getObject($documentP, 'file_url') }}', type:'pdf', showModal:true})"><i class="glyphicon glyphicon-print"></i></a>
+                                    <a type="button" class="btn" onclick="printJS({printable: '{{ Common::getObject($documentD, 'file_url') }}', type:'pdf', showModal:true})"><i class="glyphicon glyphicon-print"></i></a>
                                 @endif
                             @endif
                         </td>
