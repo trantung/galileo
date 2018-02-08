d5bf8a1faf0927a7e68b630d8d3d788b78cd8031
Tien Dung <nguyentiendung13219852017@gmail.com>
2018-02-08 13:51:52 -0800

    fix studen

--- app/views/student/create.blade.php
+++ app/views/student/create.blade.php
@@ -14,9 +14,9 @@
 @section('content')
 
 <div class="row margin-bottom">
-  <div class="col-xs-12">
-    <a href="{{ action('StudentController@index') }}" class="btn btn-success">Danh sách học sinh</a>
-</div>
+    <div class="col-xs-12">
+        <a href="{{ action('StudentController@index') }}" class="btn btn-success">Danh sách học sinh</a>
+    </div>
 </div>
 
 <div class="row">
@@ -85,82 +85,109 @@
             </fieldset>
         </div>
 
-    <div class="form-group">
-        <label for="date_study">Ngày nhập học</label>
+        <div class="box alert">
+            
+                <div class="input-group inline-block">
+                    <label style="display: block;">Lớp</label>
+                    {{ Form::select('class_id', ['' => '--Tất cả--'] + Common::getClassList(), Input::get('class_id'), ['class' => 'form-control select-class']) }}
+                </div>
+                <div class="input-group inline-block">
+                    <label style="display: block;">Môn học</label>
+                    {{ Form::select('subject_id', ['' => '--Tất cả--'] + Common::getSubjectList(), Input::get('subject_id'), ['class' => 'form-control select-subject']) }}
+                </div>
+                <div class="input-group inline-block select-level-from-class-subject">
+                    <label style="display: block;">Trình độ</label>
+                    {{ Common::getLevelDropdownList('level_id', Input::get('level_id')) }}
+                </div>
+               
+                 <div class="input-group inline-block">
+                    <label style="display: block;">Gói học</label>
+                    {{ Form::select('', ['' => '--Tất cả--', 1 => 'Đã kiểm duyệt', 2 => 'Chưa kiểm duyệt'], Input::get('status'), ['class' => 'form-control']) }}
+                </div>
+               
+         
+        </div> {{-- enddocumet --}}
+
+        <div class="form-group">
+        <label for="date_study"></label>
         <input type="date" class="form-control" id="date_study" placeholder="Ngày vào học" name="date_study">
     </div>
 
-    <div class="form-group">
-        <label for="model_name">Nguồn</label>
-        {{  Form::text('model_name', null, array('class' => 'form-control', 'placeholder' => 'Người giới thiệu' )) }}
-    </div>
 
-    <div class="form-group">
-        <label for="birthday">Ngày sinh</label>
-        <input type="date" class="form-control" required id="birthday" placeholder="Ngày sinh" name="birthday">
-    </div>
 
-    <div class="form-group">
-        <label for="gender">Giới tính</label>
-        {{ Form::select('gender', ['null' => '--chọn--', '0' => 'nữ', '1' => 'nam'], null, ['class' => 'form-control']) }}
-    </div>
 
-    <div class="form-group">
-        <label for="address">Địa chỉ</label>
-        {{  Form::text('address', null, array('class' => 'form-control', 'placeholder' => 'Địa chỉ' )) }}
-    </div>
+        <div class="form-group">
+            <label for="date_study">Ngày nhập học</label>
+            <input type="date" class="form-control" id="date_study" placeholder="Ngày vào học" name="date_study">
+        </div>
 
-    <div class="form-group">
-        <label for="phone">Số điện thoại</label>
-        {{  Form::text('phone', null, array('class' => 'form-control', 'placeholder' => 'Số điện thoại' )) }}
-    </div>
+        <div class="form-group">
+            <label for="model_name">Nguồn</label>
+            {{  Form::text('model_name', null, array('class' => 'form-control', 'placeholder' => 'Người giới thiệu' )) }}
+        </div>
 
-    <div class="form-group">
-        <label for="school">Trường học</label>
-        {{  Form::text('school', null, array('class' => 'form-control', 'placeholder' => 'Trường học' )) }}
-    </div>
-    
-    <div class="form-group">
-        <label for="link_fb">Link facebook</label>
-        {{  Form::text('link_fb', null, array('class' => 'form-control', 'placeholder' => 'Link facebook' )) }}
-    </div>
+        <div class="form-group">
+            <label for="birthday">Ngày sinh</label>
+            <input type="date" class="form-control" required id="birthday" placeholder="Ngày sinh" name="birthday">
+        </div>
 
-    <div class="form-group">
-        <label for="description">Mục tiêu</label>
-        {{  Form::text('description', null, array('class' => 'form-control', 'placeholder' => 'Mục tiêu' )) }}
-    </div>
+        <div class="form-group">
+            <label for="gender">Giới tính</label>
+            {{ Form::select('gender', ['null' => '--chọn--', '0' => 'nữ', '1' => 'nam'], null, ['class' => 'form-control']) }}
+        </div>
 
-    <div class="form-group">
-        <label for="time_target">Thời gian để đạt mục tiêu</label>
-        {{  Form::text('time_target', null, array('class' => 'form-control', 'placeholder' => 'Thời gian để đạt mục tiêu' )) }}
-    </div>
+        <div class="form-group">
+            <label for="address">Địa chỉ</label>
+            {{  Form::text('address', null, array('class' => 'form-control', 'placeholder' => 'Địa chỉ' )) }}
+        </div>
 
-    <div class="form-group">
-        <label for="info_user">Thông tin người đón</label>
-        {{  Form::text('info_user', null, array('class' => 'form-control', 'placeholder' => 'Thông tin người đón' )) }}
-    </div>
+        <div class="form-group">
+            <label for="phone">Số điện thoại</label>
+            {{  Form::text('phone', null, array('class' => 'form-control', 'placeholder' => 'Số điện thoại' )) }}
+        </div>
 
-    <div class="form-group">
-        <label for="comment">Lưu ý về học sinh</label>
-        {{ Form::textarea('comment', '', ['class' => 'form-control', 'placeholder' => 'Lưu ý về học sinh' , 'rows'=>3]) }}
-    </div>
+        <div class="form-group">
+            <label for="school">Trường học</label>
+            {{  Form::text('school', null, array('class' => 'form-control', 'placeholder' => 'Trường học' )) }}
+        </div>
+        
+        <div class="form-group">
+            <label for="link_fb">Link facebook</label>
+            {{  Form::text('link_fb', null, array('class' => 'form-control', 'placeholder' => 'Link facebook' )) }}
+        </div>
 
-    <div class="form-group">
-        <label for="program_current">Chương trình đang học</label>
-        {{  Form::text('program_current', null, array('class' => 'form-control', 'placeholder' => 'Chương trình đang học' )) }}
-    </div>
+        <div class="form-group">
+            <label for="description">Mục tiêu</label>
+            {{  Form::text('description', null, array('class' => 'form-control', 'placeholder' => 'Mục tiêu' )) }}
+        </div>
 
-</div>
-<!-- /.box-body -->
-<div class="clearfix clear"></div>
-<div class="box-footer">
-    <input type="submit" class="btn btn-primary" value="Lưu lại" />
-    <input type="reset" class="btn btn-default" value="Nhập lại" />
-</div>
-{{ Form::close() }}
-</div>
-<!-- /.box -->
-</div>
-</div>
+        <div class="form-group">
+            <label for="time_target">Thời gian để đạt mục tiêu</label>
+            {{  Form::text('time_target', null, array('class' => 'form-control', 'placeholder' => 'Thời gian để đạt mục tiêu' )) }}
+        </div>
+
+        <div class="form-group">
+            <label for="info_user">Thông tin người đón</label>
+            {{  Form::text('info_user', null, array('class' => 'form-control', 'placeholder' => 'Thông tin người đón' )) }}
+        </div>
+
+        <div class="form-group">
+            <label for="comment">Lưu ý về học sinh</label>
+            {{ Form::textarea('comment', '', ['class' => 'form-control', 'placeholder' => 'Lưu ý về học sinh' , 'rows'=>3]) }}
+        </div>
+
+       
+    </div>
+    <!-- /.box-body -->
+    <div class="clearfix clear"></div>
+    <div class="box-footer">
+        <input type="submit" class="btn btn-primary" value="Lưu lại" />
+        <input type="reset" class="btn btn-default" value="Nhập lại" />
+    </div>
+    {{ Form::close() }}
+    </div>
+    <!-- /.box -->
+    </div>
+    </div>
 
 @stop
