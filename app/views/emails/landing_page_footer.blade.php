db8cb6b907bd74f3e4279691ffc97c6fa516600d
tungtt2 <trantunghn196@gmail.com>
2018-04-11 23:08:18 +0700

    hoan thanh

--- app/views/emails/landing_page_footer.blade.php
+++ app/views/emails/landing_page_footer.blade.php
@@ -46,7 +46,7 @@
                     </tr>
                     <tr>
                         <td>
-                            <table style="width:100%;max-width:670px;border-collapse: collapse;background:url('http://cloud.galileo.edu.vn/image_landing/email/border-bottom-email.png') #0072bc;margin:0 auto;">
+                            <table style="width:100%;max-width:670px;border-collapse: collapse;background:url('http://cloud.galileo.edu.vn/image_landing/email/border-bottom-email.png') no-repeat #0072bc;margin:0 auto;">
                                 <tr>
                                     <td style="padding-top:15px">
                                         <div style="bottom:0;left:2px;width:100%;height:10px; no-repeat;"></div>
