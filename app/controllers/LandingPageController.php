cc8fa8d86f7e7c7f057b2159703e9703da297205
tungtt2 <trantunghn196@gmail.com>
2018-04-14 15:05:07 +0700

    fix

--- app/controllers/LandingPageController.php
+++ app/controllers/LandingPageController.php
@@ -170,50 +170,7 @@ class LandingPageController extends \BaseController {
     public function exportExcel()
     {
         $input = Input::all();
-        $objPHPExcel = new PHPExcel();
-        dd($objPHPExcel);
-
-        $activeSheet = $objPHPExcel->getActiveSheet();
-
-        $activeSheet->setCellValue('A1', 'STT')
-            ->setCellValue('B1', 'Tài Khoản')
-            ->setCellValue('C1', 'Họ Tên')
-            ->setCellValue('D1', 'Email')
-            ->setCellValue('E1', 'Phone')
-            ->setCellValue('F1', 'Date');
-        $r = 2;
-        foreach ($results as $result) {
-            $user = get_record('user', 'id', $result->userid);
-            $activeSheet->setCellValue('A'.$r, $i++)
-                ->setCellValue('B'.$r, $user->username)
-                ->setCellValue('C'.$r, $user->lastname . ' ' . $user->firstname)
-                ->setCellValue('D'.$r, $user->email)
-                ->setCellValue('E'.$r, $result->phone)
-                ->setCellValue('F'.$r, date('d/m/Y H:i:s', $result->created));
-            $r++;
-        }
-
-        $activeSheet->setTitle('Danh sách');
-        $objPHPExcel->setActiveSheetIndex(0);
-
-        $filename = "contact-dat-cho-nen-tang-2018-".time().".xls";
-        header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="'.$filename.'"');
-        header('Cache-Control: max-age=0');
-        // If you're serving to IE 9, then the following may be needed
-        header('Cache-Control: max-age=1');
-
-        // If you're serving to IE over SSL, then the following may be needed
-        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        header ('Pragma: public'); // HTTP/1.0
-
-        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-        $objWriter->save('php://output');
-
-
-
+        // $objPHPExcel = new PHPExcel();
         $data = CommonLanding::commonThongkeLanding($input);
         $data = $data->groupBy('email')
             ->groupBy('phone')
@@ -280,7 +237,7 @@ class LandingPageController extends \BaseController {
                 $flag = true;
             }
             // filter data
-            // array_walk($row, 'filterData');
+            array_walk($row, 'filterData');
             echo implode("\t", array_values($row)) . "\n";
 
         }
