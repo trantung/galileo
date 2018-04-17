a95b3d9ed016f485a09cc92d2e154a3c1c5b6855
tungtt2 <trantunghn196@gmail.com>
2018-04-17 10:32:57 +0700

    fix

--- app/views/admin/common/navbar.blade.php
+++ app/views/admin/common/navbar.blade.php
@@ -1,38 +1,39 @@
 <header class="main-header">
-	<!-- Logo -->
-	<a href="#" class="logo">
-		<!-- mini logo for sidebar mini 50x50 pixels -->
-		<span class="logo-mini">GALILEO</span>
-		<!-- logo for regular state and mobile devices -->
-		<span class="logo-lg">GALILEO</span>
-	</a>
-	<!-- Header Navbar: style can be found in header.less -->
-	<nav class="navbar navbar-static-top" role="navigation">
-		<!-- Sidebar toggle button-->
-		<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-			<span class="sr-only">Menu</span>
-		</a>
-		<div class="navbar-custom-menu">
-			<ul class="nav navbar-nav">
-		
-					<li class="user">
-						@if(Auth::admin()->get())
-						<a href="{{ action('AdminController@getResetPass', Common::resetPassAdminOrUser()) }}"><i class="fa fa-user">
-						@else
-						<a href="{{action('ManagerUserController@account_user', Common::resetPassAdminOrUser())}}"><i class="fa fa-user">
-						@endif
-						</i>Tài khoản</a>
-						@if($admin = Auth::admin()->get())
-							{{ $admin->username }}
-						@endif
-						@if($user = Auth::user()->get())
-							{{ $user->username }}
-						@endif
-					</li>
-					<li class="user">
-						<a href="{{ action('AdminController@logout') }}"><i class="fa fa-power-off"></i>Đăng xuất</a>
-					</li>
-			</ul>
-		</div>
-	</nav>
+    <!-- Logo -->
+    <a href="#" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini">GALILEO</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg">GALILEO</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Menu</span>
+        </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+        
+                    <li class="user">
+                        @if(Auth::admin()->get())
+                        <a href="{{ action('AdminController@getResetPass', Common::resetPassAdminOrUser()) }}"><i class="fa fa-user">
+                        @else
+                        <a href="{{action('ManagerUserController@account_user', Common::resetPassAdminOrUser())}}"><i class="fa fa-user">
+                        @endif
+                        </i>Tài khoản:
+                        @if($admin = Auth::admin()->get())
+                            {{ $admin->username }}
+                        @endif
+                        @if($user = Auth::user()->get())
+                            {{ $user->username }}
+                        @endif
+                        </a>
+                    </li>
+                    <li class="user">
+                        <a href="{{ action('AdminController@logout') }}"><i class="fa fa-power-off"></i>Đăng xuất</a>
+                    </li>
+            </ul>
+        </div>
+    </nav>
 </header>
