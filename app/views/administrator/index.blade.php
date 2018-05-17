dc6d53abc65f0d75cedee9d873b63032ed891ac5
Tan Nguyen <tantanb2@gmail.com>
2018-05-17 11:54:27 +0700

    fix style list admin page

--- app/views/administrator/index.blade.php
+++ app/views/administrator/index.blade.php
@@ -16,10 +16,7 @@
 			<th>Username</th>
 			<th>Email</th>
 			<th>Role</th>
-			<th>Phân quyền</th>
-			<th>Edit</th>
-			<th>Reset password</th>
-			<th>Delete</th>
+			<th>Thao tác</th>
 		</tr>
 		@foreach($data as $key => $admin)
 		<tr>
@@ -27,18 +24,12 @@
 			<td>{{ $admin->email }}</td>
 			<td>{{ Common::getRoleName($admin->role_id) }}</td>
 			<td>
-	           <a href="{{ action('AdminController@edit', $admin->id) }}" class="btn btn-primary">Phân quyền</a>
+           		<a href="{{ action('AdminController@edit', $admin->id) }}" class="btn btn-info" title="Sửa"><i class="fa fa-edit"></i></a>
+				<a href=" {{ action('AdminController@getResetPass', $admin->id) }}" title="Thay mật khẩu" class="btn btn-primary"><i class="fa fa-refresh"></i> Reset password</a>
+		   		{{ Form::open(array('method'=>'DELETE', 'action' => array('AdminController@destroy', $admin->id), 'class' => 'inline-block')) }}
+           			<button class="btn btn-danger" onclick="return confirm('Bạn có chắc chắn muốn xóa?');"><i class="fa fa-trash"></i></button>
+           		{{ Form::close() }}
 			</td>
-			<td>
-	           <a href="{{ action('AdminController@edit', $admin->id) }}" class="btn btn-primary">Edit</a>
-			</td>
-			</td>
-			<td><a href=" {{ action('AdminController@getResetPass', $admin->id) }} " class="btn btn-primary">Reset password</a></td>
-			<td>
-			   {{ Form::open(array('method'=>'DELETE', 'action' => array('AdminController@destroy', $admin->id), 'style' => 'display: inline-block;')) }}
-	           <button class="btn btn-danger" onclick="return confirm('Bạn có chắc chắn muốn xóa?');">Delete</button>
-	           {{ Form::close() }}
-			
 		</tr>
 		@endforeach
 	</table>
